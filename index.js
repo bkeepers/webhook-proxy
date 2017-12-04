82703a33f645b2911c560baf04d6c2ce97c59954
Brandon Keepers <bkeepers@github.com>
2017-12-04 14:24:52 -0600

    Refactor

--- index.js
+++ index.js
@@ -2,41 +2,58 @@ const sse = require('connect-sse')
 const express = require('express')
 const crypto = require('crypto')
 const bodyParser = require('body-parser')
+const EventEmitter = require('events')
 
 const app = express()
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
-const clients = {}
+const events = new EventEmitter()
 
 app.get('/', (req, res) => {
-  const token = crypto.randomBytes(12).toString('base64');
-  res.redirect(token);
+  const channel = crypto.randomBytes(12).toString('base64')
+  res.redirect(channel)
 })
 
-app.get('/:token', (req, res) => {
-  res.render('index', {token: req.params.token})
-})
+app.get('/:channel',
+  // Render HTML page if client accepts HTML
+  (req, res, next) => {
+    if (req.accepts('html')) {
+      const url = `${req.protocol}://${req.get('host')}/${req.params.channel}`
+      res.render('index', { url })
+    } else {
+      next()
+    }
+  },
 
-app.post('/:token', (req, res) => {
-  const client = clients[req.params.token]
-  if (client) {
-    console.log('passing on webhook', req.body)
-    client.json(req.body)
-    res.status(200)
-  } else {
-    res.status(404).end('No client listening for ' + req.params.token)
+  // Otherwise setup SSE
+  sse(),
+
+  // Dispatch events for this channel
+  (req, res) => {
+    // Allow CORS
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    const channel = req.params.channel
+
+    // Listen for events on this channel
+    events.on(channel, res.json)
+
+    res.on('close', () => {
+      events.removeListener(channel, res.json)
+      console.log('Client disconnected', channel, events.listenerCount(channel))
+    })
+
+    console.log('Client connected', channel, events.listenerCount(channel))
   }
-  res.end()
-})
+)
 
-app.get('/:token/events', sse(), (req, res) => {
-  clients[req.params.token] = res
-  console.log('connected', req.params.token)
-
-  // TODO:
-  // - delete from clients on disconnect
-  // - allow multipl clients to connect to same token
+app.post('/:channel', (req, res) => {
+  events.emit(req.params.channel, {
+    ...req.headers,
+    body: req.body
+  })
+  res.status(200).end()
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
